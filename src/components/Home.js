5d45e4750ab916b25627dc26516e2db68beac4aa
Harremking0 <chakibou1@hotmail.com>
2021-06-10 21:04:53 +0100

    aadded basic structure for HOme component

--- src/components/Home.js
+++ src/components/Home.js
@@ -2,8 +2,17 @@ import React from 'react'
 
 const Home = () => {
     return (
-        <div>
-            
+        <div className="main">
+            <div className="title">
+                <h1>Welcome to the Shop!</h1>
+                <p>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus mi eget auctor rutrum. Etiam sit amet dui elementum,
+                     dignissim neque eget, efficitur sapien. Fusce volutpat quis diam vel laoreet.
+                     Aenean quis imperdiet odio.
+                      Duis vel nisi eu nulla dignissim faucibus.
+                       Suspendisse lorem lacus, egestas sodales ultrices ve
+                       </p>
+            </div>
         </div>
     )
 }
