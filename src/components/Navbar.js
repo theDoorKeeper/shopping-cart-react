f5953b05592dc9985f32350eb3b38177dd03ab7c
Harremking0 <chakibou1@hotmail.com>
2021-06-10 18:32:23 +0100

    basic structure for navbar using NavLink

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,9 +1,21 @@
 import React from 'react'
-
+import NavLink from "react-router-dom";
 const Navbar = () => {
     return (
         <div>
-            
+            <ul>
+                <li>
+                    <NavLink activeClassName="active" className="nav-element" exact to="/"> home </NavLink>
+                </li>
+
+                <li>
+                <NavLink activeClassName="active" className="nav-element" exact to="/shop"> shop </NavLink>
+                </li>
+
+                <li>
+                <NavLink activeClassName="active" className="nav-element" exact to="/cart"> cart </NavLink>
+                </li>
+            </ul>
         </div>
     )
 }
