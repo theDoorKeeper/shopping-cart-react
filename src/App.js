1af4e8fc3789dec8dde569591823771c56ce5714
Harremking0 <chakibou1@hotmail.com>
2021-06-10 18:08:17 +0100

    added the react router dom package

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
+import Navbar from "./components/Navbar";
+
 function App() {
   return (
     <div className="App">
+      <Navbar/>
     </div>
-  );
+  ); 
 }
 
 export default App;
