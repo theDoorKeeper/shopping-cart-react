448a3d6cc9b0166c7dd8586b525d0a870ab3e034
Harremking0 <chakibou1@hotmail.com>
2021-06-11 03:09:01 +0100

    added two buttons for categories in Store component

--- src/components/Store.js
+++ src/components/Store.js
@@ -1,22 +1,24 @@
 import React, { useEffect, useState } from 'react'
 
 const Store = () => {
-        const [category,setCategory]=useState('featured')
+        const [category,setCategory]=useState('daily')
         const [itemArray,setItemArray]=useState([])
 
-        const getData = async ()=>{
+        const getData = async (category)=>{
            const response = await fetch("https://fortnite-api.com/v2/shop/br",{mode:'cors'})
            const jsonfile = await response.json()
-           setItemArray( jsonfile.data.featured.entries )
+           setItemArray( jsonfile.data[category].entries )
         }
     useEffect(() => {
-            getData();
+          getData(category) ;
             console.log(itemArray)
 
-    }, [])
+    }, [category])
 
     return (
         <div className="Store">
+            <button className="category-btn" id="daily"> daily</button>
+            <button className="category-btn" id="featured"> featured</button>
             
         </div>
     )
