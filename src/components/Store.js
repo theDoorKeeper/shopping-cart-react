2d6f43d5ea0f3b680a1ef6b8826965625d5f7868
Harremking0 <chakibou1@hotmail.com>
2021-06-11 16:09:16 +0100

    added details for every item , and trniary operation to check if it has a feature icon , pick another icon if false

--- src/components/Store.js
+++ src/components/Store.js
@@ -22,9 +22,10 @@ const Store = () => {
                 setDisplayedItems(itemArray.map((item,index) => {
                     return <div key={index} className="card">
 
-                        <div className="card-title">{item.items[0].name}</div>
-                        <img src={item.items[0].images.featured} alt="item icon"/>
-
+                        <div className="card-title"><h2>{item.items[0].name}</h2></div>
+                        <img src={item.items[0].images.featured ? item.items[0].images.featured : item.items[0].images.icon} alt="item icon"/>
+                        <div className="card-details"><h5>{item.items[0].description}</h5></div>
+                        <div className="card-price"><h6>{item.regularPrice} <img src={"https://fortnite-api.com/images/vbuck.png"} alt="vbuck icon"/></h6></div>
                     </div>
                 }))
             }, [itemArray]) 
