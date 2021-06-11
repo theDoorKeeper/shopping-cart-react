2fdf23c48a2d7f1e41e85b950cc70c806eed0753
Harremking0 <chakibou1@hotmail.com>
2021-06-11 02:33:07 +0100

    added some dummy text and class

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,20 +1,18 @@
 import React from 'react'
 import logo from "../assets/logo.svg"
+import "./Home.css"
 const Home = () => {
     return (
         <div className="main">
             <div className="title">
                 <h1>Welcome to the Shop!</h1>
                 <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer maximus mi eget auctor rutrum. Etiam sit amet dui elementum,
-                     dignissim neque eget, efficitur sapien. Fusce volutpat quis diam vel laoreet.
-                     Aenean quis imperdiet odio.
-                      Duis vel nisi eu nulla dignissim faucibus.
-                       Suspendisse lorem lacus, egestas sodales ultrices ve
-                       </p>
+                    Take a look at our discounted new Skins!<br></br>
+                    brag to your friends with you rare shiny Backpack
+                 </p>
             </div>
             <div className="logo">
-                <img src={logo} alt="fortnine logo"/>
+                <img src={logo} alt="fortnine logo" className="filter"/>
             </div>
         </div>
     )
