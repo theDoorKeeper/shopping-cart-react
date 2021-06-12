0a0455737b0c3ac9d3ba45c2dbb4ee997cc145ec
Harremking0 <chakibou1@hotmail.com>
2021-06-12 05:19:13 +0100

    added useEffect logic that populated the display depending on the cartArray prop

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -5,13 +5,25 @@ const Cart = (props) => {
     const {cartArray} = props;
 
     useEffect(() => {
-        console.log(cartArray)
+        setDisplayedCart(cartArray.map(( data,index ) => {
+          return  <div key={index} className="card" id={data.items[0].id}>
+                        <div className="card-title"><h2>{data.items[0].name}</h2></div>
+                        <img className="card-icon" src={data.items[0].images.featured ? data.items[0].images.featured : data.items[0].images.icon} alt="item icon"/>
+                        <div className="card-details"><h5>{data.items[0].description}</h5> <br/> </div>
+                        <div className="card-price"><h6>{data.regularPrice} <img className="vbuck-icon"src={"https://fortnite-api.com/images/vbuck.png"} alt="vbuck icon"/>
+                        </h6>
+                        
+                        </div>
+                        
+                    </div>
+
+        }))
 
     }, [cartArray])
 
     return (
         <div>
-            
+            {displayedCart}
         </div>
     )
 }
