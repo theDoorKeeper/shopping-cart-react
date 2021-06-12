4a7b3572e1611590e0ea35288319209b9c4a742c
Harremking0 <chakibou1@hotmail.com>
2021-06-12 01:33:44 +0100

    aded cartArray usestate , and function to papoluate it on click

--- src/components/Store.js
+++ src/components/Store.js
@@ -4,11 +4,17 @@ const Store = () => {
         const [category,setCategory]=useState('daily')
         const [itemArray,setItemArray]=useState([])
         const [displayedItems,setDisplayedItems]=useState([])
+        const [cartArray,setCartArray]=useState([]);
+
 
         const getData = async (category)=>{
            const response = await fetch("https://fortnite-api.com/v2/shop/br",{mode:'cors'})
            const jsonfile = await response.json()
            setItemArray( jsonfile.data[category].entries )
+        }
+
+        const addToCart =(item)=>{
+                setCartArray(cartArray.concat(item))
         }
 
 
@@ -24,7 +30,7 @@ const Store = () => {
                         <img className="card-icon" src={item.items[0].images.featured ? item.items[0].images.featured : item.items[0].images.icon} alt="item icon"/>
                         <div className="card-details"><h5>{item.items[0].description}</h5> <br/> </div>
                         <div className="card-price"><h6>{item.regularPrice} <img className="vbuck-icon"src={"https://fortnite-api.com/images/vbuck.png"} alt="vbuck icon"/>
-                        <button className="card-buy" id={item.items[0].id}>Add to cart</button>
+                        <button className="card-buy " id={item.items[0].id} onClick={()=>{addToCart(item)}}> <span>Add to cart </span></button>
                         </h6>
                         
                         </div>
@@ -41,10 +47,10 @@ const Store = () => {
     return (
         <div className="store">
             <div className="buttons">
-            <button className="category-btn" id="daily" onClick={()=>{setCategory('daily')}}> daily</button>
-            <button className="category-btn" id="special-daily" onClick={()=>{setCategory('specialDaily')}}> Special daily</button>
-            <button className="category-btn" id="featured" onClick={()=>{setCategory('featured')}}> featured</button>
-            <button className="category-btn" id="special-featured" onClick={()=>{setCategory('specialFeatured')}}> Special featured</button>
+            <button className="category-btn" id="daily" onClick={()=>{setCategory('daily')}}> <span>daily</span> </button>
+            <button className="category-btn" id="special-daily" onClick={()=>{setCategory('specialDaily')}}> <span>Special daily</span> </button>
+            <button className="category-btn" id="featured" onClick={()=>{setCategory('featured')}}> <span>featured</span> </button>
+            <button className="category-btn" id="special-featured" onClick={()=>{setCategory('specialFeatured')}}><span> Special featured</span> </button>
             </div>
             <div className="catalogue">
             {displayedItems}
