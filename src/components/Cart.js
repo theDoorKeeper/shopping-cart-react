78bf8958e7b601b2ac726917d62a3874ce0037d9
Harremking0 <chakibou1@hotmail.com>
2021-06-14 23:33:02 +0100

    structuring the diplsay of the cart array

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -4,13 +4,13 @@ import React, { useEffect, useState } from 'react';
 
 const Cart = (props) => {
   const [displayedCart, setDisplayedCart] = useState([]);
-  const { cartArray, incrementItem  } = props;
+  const { cartArray, incrementItem ,decrementItem, deleteItem } = props;
   
   useEffect(() => {
     setDisplayedCart(cartArray.map((data) => (
-      <div key={data.item.offerId} className="card" id={data.item.offerId}>
+      <div key={data.item.offerId} className="checkout-card" id={data.item.offerId}>
         <div className="card-title"><h2>{data.item.items[0].name}</h2></div>
-        <img className="card-icon" src={data.item.items[0].images.featured ? data.item.items[0].images.featured : data.item.items[0].images.icon} alt="item icon" />
+        <img className="card-icon" src={/* data.item.items[0].images.featured ? data.item.items[0].images.featured : */ data.item.items[0].images.icon} alt="item icon" />
         <div className="card-details">
           <h5>{data.item.items[0].description}</h5>
           {' '}
@@ -24,17 +24,22 @@ const Cart = (props) => {
             <img className="vbuck-icon" src="https://fortnite-api.com/images/vbuck.png" alt="vbuck icon" />
            <button type='button' onClick={()=>{ incrementItem(cartArray,data.item.offerId) } }>increment</button>
             {data.amount}
+            <button type='button' onClick={()=>{ decrementItem(cartArray,data.item.offerId) } }>decrement</button>
+            <button type='button' onClick={()=>{ deleteItem(cartArray,data.item.offerId) } }>delete</button>
           </h6>
 
         </div>
 
       </div>
     )));
-  }, [cartArray,incrementItem]);
+  }, [cartArray,incrementItem,decrementItem,deleteItem]);
 
   return (
-    <div>
+    <div className="cart-wrapper" >
+      <div className="boughtItems">
       {displayedCart}
+      </div>
+     
     </div>
   );
 };
