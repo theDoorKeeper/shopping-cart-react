77291280a8b083892b968243aa837b3c38216c52
Harremking0 <chakibou1@hotmail.com>
2021-06-15 02:24:06 +0100

    added styling for Cart component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,6 +1,7 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-filename-extension */
 import React, { useEffect, useState } from 'react';
+import './Cart.css'
 
 const Cart = (props) => {
   const [displayedCart, setDisplayedCart] = useState([]);
@@ -9,35 +10,34 @@ const Cart = (props) => {
   useEffect(() => {
     setDisplayedCart(cartArray.map((data) => (
       <div key={data.item.offerId} className="checkout-card" id={data.item.offerId}>
-        <div className="card-title"><h2>{data.item.items[0].name}</h2></div>
-        <img className="card-icon" src={/* data.item.items[0].images.featured ? data.item.items[0].images.featured : */ data.item.items[0].images.icon} alt="item icon" />
-        <div className="card-details">
-          <h5>{data.item.items[0].description}</h5>
-          {' '}
-          <br />
-          {' '}
-        </div>
-        <div className="card-price">
-          <h6>
-            {data.item.regularPrice}
-            {' '}
-            <img className="vbuck-icon" src="https://fortnite-api.com/images/vbuck.png" alt="vbuck icon" />
-           <button type='button' onClick={()=>{ incrementItem(cartArray,data.item.offerId) } }>increment</button>
+        <div className="checkout-title"> 
+        <div className="checkout-name" >{data.item.items[0].name} </div>
+         <button type='button' className="delete-btn"  onClick={()=>{ deleteItem(cartArray,data.item.offerId) } }>x</button>
+        </div>           
+       <img className="checkout-icon" src={/* data.item.items[0].images.featured ? data.item.items[0].images.featured : */ data.item.items[0].images.icon} alt="item icon" />
+        <div className="buttons">
+          <button type='button' className="checkout-btn" onClick={()=>{ decrementItem(cartArray,data.item.offerId) } }>-</button>
             {data.amount}
-            <button type='button' onClick={()=>{ decrementItem(cartArray,data.item.offerId) } }>decrement</button>
-            <button type='button' onClick={()=>{ deleteItem(cartArray,data.item.offerId) } }>delete</button>
-          </h6>
+             <button type='button' className="checkout-btn" onClick={()=>{ incrementItem(cartArray,data.item.offerId) } }>+</button>
+        </div>
+        <div className="checkout-price">
+            {' '}{ (data.item.regularPrice)*(data.amount) }
+            <img className="vbuck-icon-checkout" src="https://fortnite-api.com/images/vbuck.png" alt="vbuck icon" /> 
 
         </div>
-
       </div>
     )));
   }, [cartArray,incrementItem,decrementItem,deleteItem]);
 
   return (
     <div className="cart-wrapper" >
-      <div className="boughtItems">
+      <div className="bought-items">
       {displayedCart}
+      </div>
+
+      <div className="checkout-sidebar">
+        <div className="sidebar-title">Checkout:</div>
+        
       </div>
      
     </div>
