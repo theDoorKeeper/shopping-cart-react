97a40dd9d37c7e8251bd20127355ee60847ccdd2
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-29 02:44:28 +0100

    added loading state to load after setting suer

--- src/components/AuthProvider.js
+++ src/components/AuthProvider.js
@@ -1,23 +1,29 @@
 import React, { useEffect } from 'react';
 import { useContext, useState } from 'react/cjs/react.development';
-import { auth } from "../../firebase";
+import { auth } from "../firebase";
     const AuthContext = React.createContext();
 
     export const useAuth = ()=>{
         return useContext(AuthContext)
     }
 
-function AuthProvider({children}) {
+export function AuthProvider({children}) {
     const [currentUser, setcurrentUser] = useState();
+    const [loading, setloading] = useState(true);
 
     const signUp = (email, password )=>{
         return   auth.createUserWithEmailAndPassword(email, password)
       }
+
+
       
       useEffect(() => {
+          
       const unsubScribe =  auth.onAuthStateChanged(user=>{
           setcurrentUser(user)
-      })     
+      });
+
+      setloading(false);
           return () => unsubScribe
       }, [])
      
@@ -27,9 +33,9 @@ function AuthProvider({children}) {
     }
     return (
         <AuthContext.Provider value={value}>
-            {children}
+            {!loading && children}
         </AuthContext.Provider>
     )
 }
 
-export default AuthProvider
+
