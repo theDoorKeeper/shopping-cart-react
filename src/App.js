50e53ae0e9ee1b262e46cd646e34fa5184e1258c
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-29 23:51:37 +0100

    fixed merging issue

--- src/App.js
+++ src/App.js
@@ -14,8 +14,6 @@ import Login from './components/Login';
 
 function App() {
   return (
-<<<<<<< HEAD
-  
      <Router> 
       <AuthProvider>
       <Switch >
@@ -27,38 +25,7 @@ function App() {
   
      </Router> 
  
-=======
-    <Router>
-      <div className="App">
-        <Navbar getItemsNumber={getItemsNumber} totalItems={totalItems} cartArray={cartArray} />
 
-        <Switch>
-
-          <Route exact path="/shopping-cart-react">
-            <Home />
-          </Route>
-
-          <Route exact path="/shop">
-            <Store addToCart={addToCart} />
-          </Route>
-
-          <Route exact path="/cart">
-            <Cart
-              cartArray={cartArray}
-              incrementItem={incrementItem}
-              decrementItem={decrementItem}
-              deleteItem={deleteItem}
-              totalPrice={totalPrice}
-              totalItems={totalItems}
-              getItemsNumber={getItemsNumber}
-              giveTotalPrice={giveTotalPrice}
-            />
-          </Route>
-
-        </Switch>
-      </div>
-    </Router>
->>>>>>> 853399391bafc8a94072903336e23b9c1087ffa1
   );
 }
 
