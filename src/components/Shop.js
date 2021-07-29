b013b097b220657989c238a881f7d408fb9a61bf
thteDoorKeeper <chakibou1@hotmail.com>
2021-07-29 03:58:04 +0100

    added handlelogout fucntion to catch errors and added useHistory to redirect

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,23 +1,26 @@
 /* eslint-disable no-param-reassign */
 /* eslint-disable react/jsx-filename-extension */
+import "./Shop.css"
 import {
     BrowserRouter as Router,
     Switch,
     Route,
+    useHistory,
   } from 'react-router-dom';
   import React, { useState } from 'react';
-  import Navbar from './components/Navbar';
-  import './App.css';
-  import Home from './components/Home';
-  import Store from './components/Store';
-  import Cart from './components/Cart';
-  import { AuthProvider } from './components/AuthProvider';
+  import Navbar from './Navbar';
+  import Home from './Home';
+  import Store from './Store';
+  import Cart from './Cart';
+  import { AuthProvider, useAuth } from './AuthProvider';
   
   function Shop() {
     const [cartArray, setCartArray] = useState([]);
     const [totalPrice, setTotalPrice] = useState(0);
     const [totalItems, setTotalItems] = useState(0);
-  
+    const [error, setError]=useState("");
+    const { currentUser, logout } = useAuth();  
+    const history = useHistory();
     const addToCart = (item) => {
       setCartArray((prvState) => {
         if (prvState.filter((element) => element.item.offerId === item.offerId).length) {
@@ -81,15 +84,28 @@ import {
       setTotalItems(number);
     };
   
+    const handleLogout = async () =>{ 
+            setError("")
+            try{
+                await logout()
+                history.push("/login") 
+            }
+            catch(err){
+                setError(err.message)
+            }
+        }
     return (
-    <AuthProvider>
+    
        <Router>
+           <AuthProvider>
         <div className="App">
           <Navbar getItemsNumber={getItemsNumber} totalItems={totalItems} cartArray={cartArray} />
-  
+            welcome {currentUser && currentUser.email}
+            <button onClick={()=>handleLogout()}> log Out </button>
+            {error}
           <Switch>
   
-            <Route exact path="/Home">
+            <Route exact path="/home">
               <Home />
             </Route>
   
@@ -112,8 +128,9 @@ import {
   
           </Switch>
         </div>
+         </AuthProvider>
       </Router> 
-          </AuthProvider>
+         
     );
   }
   
